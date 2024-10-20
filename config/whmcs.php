b6a71bdb267202202da7dbcc460619d8170c0bac
Eugene van der Merwe <eugene@vander.host>
2024-10-20 11:00:07 +0200

    add admin url also

--- config/whmcs.php
+++ config/whmcs.php
@@ -2,6 +2,7 @@
 
 return [
     'url' => env('WHMCS_URL'),
+    'admin_url' => env('WHMCS_ADMIN_URL'),
     'api_identifier' => env('WHMCS_API_IDENTIFIER'),
     'api_secret' => env('WHMCS_API_SECRET'),
     'limitnum' => env('WHMCS_LIMITNUM', 10000),
