1fee24efe48477754346cf023c4f4319777c5be3
Eugene van der Merwe <eugene@vander.host>
2024-10-20 11:36:20 +0200

    more whmcs defaults

--- config/whmcs.php
+++ config/whmcs.php
@@ -5,6 +5,7 @@ return [
     'admin_url' => env('WHMCS_ADMIN_URL'),
     'api_identifier' => env('WHMCS_API_IDENTIFIER'),
     'api_secret' => env('WHMCS_API_SECRET'),
-    'limitnum' => env('WHMCS_LIMITNUM', 10000),
     'debug' => env('WHMCS_DEBUG'),
+    'limitnum' => env('WHMCS_LIMITNUM', 10000),
+    'default_payment_method' => env('WHMCS_DEFAULT_PAYMENT_METHOD', 'mailin'),
 ];
